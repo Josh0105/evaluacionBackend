d929ba7f0c7b3b5f4c3de1d7f7410700247de341
Jonathan Calo <j_2_calo@hotmail.com>
2022-05-15 23:13:42 -0600

    se eliminan clases inservibles de coneccion SOAP

--- src/main/java/com/evaluacion/servidor/com/claro/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://claro.com/")
-package com.evaluacion.servidor.com.claro;
--- src/main/java/com/evaluacion/servidor/controllers/UserController.java
+++ src/main/java/com/evaluacion/servidor/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.evaluacion.servidor.controllers;
 
-import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.ArrayList;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
